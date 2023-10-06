ee70a88ff7ea14f7e96c5a08c6ebc8bb34eeca18
Tomasz Pawlik <tomasz.pawlik@ciklum.com>
2023-10-06 16:43:09 +0200

    change ws port

--- src/events/events.gateway.ts
+++ src/events/events.gateway.ts
@@ -9,7 +9,7 @@ import { from, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Server } from 'socket.io';
 
-@WebSocketGateway(80, {})
+@WebSocketGateway(9090, {})
 export class EventsGateway {
   @WebSocketServer()
   server: Server;
