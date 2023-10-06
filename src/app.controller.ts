2483acd8c5fa40b34319cdb6d9033c22537e1930
Tomasz Pawlik <tomasz.pawlik@ciklum.com>
2023-10-06 16:35:10 +0200

    add basic socket.io config

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
+  @Post('/webhook')
+  getHello(@Body() data): string {
     return this.appService.getHello();
   }
 }
