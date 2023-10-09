ab509c26b9b2e530203881f29c3cb811a5205e09
Tomasz Pawlik <tomasz.pawlik@ciklum.com>
2023-10-09 10:43:50 +0200

    change cors options and event name

--- src/events/events.gateway.ts
+++ src/events/events.gateway.ts
@@ -9,15 +9,20 @@ import { from, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Server } from 'socket.io';
 
-@WebSocketGateway({})
+@WebSocketGateway({
+  cors: {
+    origin: '*',
+  },
+})
 export class EventsGateway {
   @WebSocketServer()
   server: Server;
 
-  @SubscribeMessage('payment-notifications')
+  @SubscribeMessage('payments')
   findAll(@MessageBody() data: any): Observable<WsResponse<number>> {
+    console.log('hurray');
     return from([1, 2, 3]).pipe(
-      map((item) => ({ event: 'events', data: item })),
+      map((item) => ({ event: 'payments', data: item })),
     );
   }
 }
